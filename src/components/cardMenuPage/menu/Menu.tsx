b2af31427a4d24e38feda91290cd8e043d4c3a2d
IsoardIMarius <misoardi13100@outlook.fr>
2023-10-17 22:14:18 +0200

    change architecture

--- src/components/cardMenuPage/menu/Menu.tsx
+++ src/components/cardMenuPage/menu/Menu.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import {renderCardProductsWithCategory} from "../card/CardService";
 import {renderProductsWithoutCategory} from "../product/ProductService";
 import api from "../../../dataBrut/api.json";
 import {fetchCategoryByMenuId, fetchMenuProduct} from "./MenuApi";
--- src/components/cardMenuPage/menu/MenuService.tsx
+++ src/components/cardMenuPage/menu/MenuService.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 import Product from "../product/Product";
 import Menu from "./Menu";
 
