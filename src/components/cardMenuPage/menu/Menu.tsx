0a4cd3389648193485c9791d1f37918878557e84
IsoardIMarius <misoardi13100@outlook.fr>
2023-10-19 15:28:39 +0200

    change color description menu

--- src/components/cardMenuPage/menu/Menu.tsx
+++ src/components/cardMenuPage/menu/Menu.tsx
@@ -28,7 +28,7 @@ const Menu = ({menuData}: any) => {
         <div id={`${menuData.name}`} className="boxShadow">
             <section
                 className="bg-bgGray rounded-md font-extrabold text-center flex flex-col justify-center pt-12 pb-8 px-4 md:!px-16 lg:!px-44 mx-3 my-4 md:!mx-8 !scroll-smooth">
-                <h2 className="text-xl md:text-2xl mb-4 uppercase self-center">{menuData.name} {menuData.price}€</h2>
+                <h2 className="text-xl md:text-2xl mb-8 uppercase self-center">{menuData.name} {menuData.price}€</h2>
                 <p className={"mb-8 text-grayLight"}>{menuData.description}</p>
                 {!hasCategories && !isFirstCategoryNull ? (
                     <p className={"text-xl"}>A venir ..</p>
